877a8b4dbdbf594ce29a380c24270bf7b5e24ffb
Tim Goll <github@timgoll.de>
2023-11-02 14:11:58 +0100

    added boilerplate UI

---
+++ plugins/menu.py
@@ -0,0 +1,45 @@
+menu_structure = [
+	{
+		"type" : "choices",
+		"label" : "layer count",
+		"choices" : ["1 layer", "2 layers", "3 layers"],
+		"default" : 0,
+		"unit" : None
+	},{
+		"type" : "text",
+		"label" : "turns per layer",
+		"default" : 12,
+		"unit" : None
+	},{
+		"type" : "text",
+		"label" : "outer diameter",
+		"default" : 12,
+		"unit" : "mm"
+	},{
+		"type" : "choices",
+		"label" : "turn direction",
+		"choices" : ["clockwise", "counter clockwise"],
+		"default" : 0,
+		"unit" : None
+	},{
+		"type" : "text",
+		"label" : "trace width",
+		"default" : 0.127,
+		"unit" : "mm"
+	},{
+		"type" : "text",
+		"label" : "trace spacing",
+		"default" : 0.127,
+		"unit" : "mm"
+	},{
+		"type" : "text",
+		"label" : "via outer diameter",
+		"default" : 0.6,
+		"unit" : "mm"
+	},{
+		"type" : "text",
+		"label" : "via drill diameter",
+		"default" : 0.3,
+		"unit" : "mm"
+	}
+]
---
+++ plugins/plugin.py
@@ -0,0 +1,128 @@
+import os
+import wx # type: ignore
+import pcbnew # type: ignore
+
+from .menu import *
+
+# WX GUI form that show coil settings
+class CoilGeneratorUI(wx.Frame):
+	def __init__(self):
+		self.width_label = 120
+		self.width_content = 180
+		self.padding = 5
+
+		wx.Dialog.__init__(
+			self,
+			None,
+			id = wx.ID_ANY,
+			title = u"Coil Generator",
+			pos = wx.DefaultPosition,
+			size = wx.DefaultSize,
+			style = wx.DEFAULT_DIALOG_STYLE
+		)
+
+		self.sizer_box = wx.BoxSizer(wx.VERTICAL)
+
+		# self.app = wx.PySimpleApp()
+		icon = wx.Icon(os.path.join(os.path.dirname(__file__), 'icon.png'))
+		self.SetIcon(icon)
+		self.SetBackgroundColour(wx.LIGHT_GREY)
+
+		for entry in menu_structure:
+			if entry["type"] == "choices":
+				self._make_choices(entry["label"], entry["choices"], entry["default"], entry["unit"])
+
+			if entry["type"] == "checkbox":
+				self._make_checkbox(entry["label"], entry["default"], entry["unit"])
+
+			if entry["type"] == "slider":
+				self._make_slider(entry["label"], entry["min"], entry["max"], entry["default"], entry["unit"])
+
+			if entry["type"] == "text":
+				self._make_textbox(entry["label"], entry["default"], entry["unit"])
+
+		elem_button_generate = wx.Button(self, label="Generate Coil")
+
+		self.sizer_box.Add(elem_button_generate, 0, wx.ALL, self.padding)
+
+		self.SetSizer(self.sizer_box)
+		self.Layout()
+		self.sizer_box.Fit(self)
+		self.Centre(wx.BOTH)
+
+	def _make_choices(self, label, choices, default = 0, unit = None):
+		elem_label = wx.StaticText(self, label=label)
+		elem_choices = wx.Choice(self, choices=choices)
+
+		elem_choices.SetSelection(default)
+
+		self._add_content(
+			elem_label,
+			elem_choices,
+			unit
+		)
+
+	def _make_checkbox(self, label, default = 0, unit = None):
+		elem_label = wx.StaticText(self, label=label)
+		elem_check = wx.CheckBox(self)
+
+		self._add_content(
+			elem_label,
+			elem_check,
+			unit
+		)
+
+	def _make_slider(self, label, min, max, default = 0, unit = None):
+		elem_label = wx.StaticText(self, label=label)
+		elem_slider = wx.Slider(self, value = 50, minValue = min, maxValue = max, style = wx.SL_HORIZONTAL | wx.SL_LABELS)
+
+		self._add_content(
+			elem_label,
+			elem_slider,
+			unit
+		)
+
+	def _make_textbox(self, label, default = 0, unit = None):
+		elem_label = wx.StaticText(self, label=label)
+		elem_text = wx.TextCtrl(self)
+
+		elem_text.SetValue(str(default))
+
+		self._add_content(
+			elem_label,
+			elem_text,
+			unit
+		)
+
+	def _add_content(self, elem_label, elem_content, unit):
+		elem_label.SetMinSize((self.width_label, -1))
+		elem_content.SetMinSize((self.width_content, -1))
+
+		sizer = wx.BoxSizer(wx.HORIZONTAL)
+		sizer.Add(elem_label, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, self.padding)
+		sizer.Add(elem_content, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, self.padding)
+
+		if unit:
+			unit_label = wx.StaticText(self, label=unit)
+
+			# decrease the content box size:
+			elem_content.SetMinSize((self.width_content - unit_label.GetSize().GetWidth() - 2 * self.padding, -1))
+
+			sizer.Add(unit_label, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, self.padding)
+
+		self.sizer_box.Add(sizer, 0, wx.ALL, self.padding)
+
+
+# Plugin definition
+class Plugin(pcbnew.ActionPlugin):
+	def __init__(self):
+		self.name = "Coil Generator"
+		self.category = "Manufacturing"
+		self.description = "Toolkit to automatically generate coils for KiCad"
+		self.pcbnew_icon_support = hasattr(self, "show_toolbar_button")
+		self.show_toolbar_button = True
+		self.icon_file_name = os.path.join(os.path.dirname(__file__), 'icon.png')
+		self.dark_icon_file_name = os.path.join(os.path.dirname(__file__), 'icon.png')
+
+	def Run(self):
+		CoilGeneratorUI().Show()
